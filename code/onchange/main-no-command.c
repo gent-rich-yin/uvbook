f762688ceb1f8d85fd9ccd7e0640d82730ad8db6
richard <gent.rich.yin@gmail.com>
2025-07-19 23:11:03 +0000

    add nochange

--- code/onchange/main-no-command.c
+++ code/onchange/main-no-command.c
@@ -4,9 +4,8 @@
 #include <uv.h>
 
 uv_loop_t *loop;
-const char *command;
 
-void run_command(uv_fs_event_t *handle, const char *filename, int events, int status) {
+void print_info(uv_fs_event_t *handle, const char *filename, int events, int status) {
     char path[1024];
     size_t size = 1023;
     // Does not handle error if path is longer than 1023.
@@ -20,27 +19,25 @@ void run_command(uv_fs_event_t *handle, const char *filename, int events, int st
         fprintf(stderr, "changed");
 
     fprintf(stderr, " %s\n", filename ? filename : "");
-    system(command);
 }
 
 /***
- * Example: ./code/onchange/onchange "ls -l /tmp" /tmp
+ * Example: ./code/onchange/onchange-no-command  /tmp
  */
 int main(int argc, char **argv) {
-    if (argc <= 2) {
-        fprintf(stderr, "Usage: %s <command> <file1> [file2 ...]\n", argv[0]);
+    if (argc <= 1) {
+        fprintf(stderr, "Usage: %s <file1> [file2 ...]\n", argv[0]);
         return 1;
     }
 
     loop = uv_default_loop();
-    command = argv[1];
 
-    while (argc-- > 2) {
+    while (argc-- > 1) {
         fprintf(stderr, "Adding watch on %s\n", argv[argc]);
         uv_fs_event_t *fs_event_req = malloc(sizeof(uv_fs_event_t));
         uv_fs_event_init(loop, fs_event_req);
         // The recursive flag watches subdirectories too.
-        uv_fs_event_start(fs_event_req, run_command, argv[argc], UV_FS_EVENT_RECURSIVE);
+        uv_fs_event_start(fs_event_req, print_info, argv[argc], UV_FS_EVENT_RECURSIVE);
     }
 
     return uv_run(loop, UV_RUN_DEFAULT);
