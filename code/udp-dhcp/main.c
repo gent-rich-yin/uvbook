1e5da9d8c9f108aa2ec46a7e3be58682d47703f4
richard <gent.rich.yin@gmail.com>
2025-07-20 18:48:27 +0000

    add udp-dhcp

--- code/udp-dhcp/main.c
+++ code/udp-dhcp/main.c
@@ -1,3 +1,7 @@
+// @TODO Write udp-dhcp-server
+// This code simulates a DHCP client sending a discovery message and getting a reply from DHCP server
+// When running it on GCP VM, no reply was received
+// We must run this program with sudo
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +12,13 @@
 uv_loop_t *loop;
 uv_udp_t send_socket;
 uv_udp_t recv_socket;
+
+void check_status(int status) {
+    if(status < 0) {
+        fprintf(stderr, "Error: %s\n", uv_err_name(status));
+        exit(-1);
+    }
+}
 
 void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
   buf->base = malloc(suggested_size);
@@ -103,25 +114,30 @@ void on_send(uv_udp_send_t *req, int status) {
 
 int main() {
     loop = uv_default_loop();
+    int status;
 
+    // DHCP client code, listens to any DHCP server replies on port 68
     uv_udp_init(loop, &recv_socket);
     struct sockaddr_in recv_addr;
     uv_ip4_addr("0.0.0.0", 68, &recv_addr);
-    uv_udp_bind(&recv_socket, (const struct sockaddr *)&recv_addr, UV_UDP_REUSEADDR);
-    uv_udp_recv_start(&recv_socket, alloc_buffer, on_read);
+    // UV_UDP_REUSEADDR = SO_REUSEADDR, share the port 68 with other processes
+    check_status(uv_udp_bind(&recv_socket, (const struct sockaddr *)&recv_addr, UV_UDP_REUSEADDR));
+    check_status(uv_udp_recv_start(&recv_socket, alloc_buffer, on_read));
 
     uv_udp_init(loop, &send_socket);
     struct sockaddr_in broadcast_addr;
+    // Port 0 means dynamic port
     uv_ip4_addr("0.0.0.0", 0, &broadcast_addr);
-    uv_udp_bind(&send_socket, (const struct sockaddr *)&broadcast_addr, 0);
-    uv_udp_set_broadcast(&send_socket, 1);
+    check_status(uv_udp_bind(&send_socket, (const struct sockaddr *)&broadcast_addr, 0));
+    check_status(uv_udp_set_broadcast(&send_socket, 1));
 
     uv_udp_send_t send_req;
     uv_buf_t discover_msg = make_discover_msg();
 
     struct sockaddr_in send_addr;
     uv_ip4_addr("255.255.255.255", 67, &send_addr);
-    uv_udp_send(&send_req, &send_socket, &discover_msg, 1, (const struct sockaddr *)&send_addr, on_send);
+    check_status(uv_udp_send(&send_req, &send_socket, &discover_msg, 1, (const struct sockaddr *)&send_addr, on_send));
 
+    printf("Starting uv loop\n");
     return uv_run(loop, UV_RUN_DEFAULT);
 }
